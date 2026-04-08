a844b2026c764f854111c3b56a06de2cc2fda573
Danilo Milivojevic <151475025+dantai9@users.noreply.github.com>
2026-04-08 20:20:53 +0200

    1768-merge-strings-alternately .py 04-08-2026

--- LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,12 +1,10 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        list1 = list(word1)
-        list2 = list(word2)
-        final_list = []
-        lenght = len(list1) + len(list2)
+        res = []
+        lenght = len(word1) + len(word2)
         for i in range(lenght):
-            if i < len(list1):  
-                final_list.append(list1[i])
-            if i < len(list2):
-             final_list.append(list2[i])
-        return ''.join(final_list)
+            if i < len(word1):  
+                res.append(word1[i])
+            if i < len(word2):
+             res.append(word2[i])
+        return ''.join(res)
